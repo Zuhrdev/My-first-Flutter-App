c77e6783995d72c23cd4a21670ff21e3c1cbc47e
elie chardin <elie.chardin@epitech.eu>
2023-02-26 14:12:38 +0100

    need u alex

---
+++ firstUI/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var platform = Theme.of(context).platform;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Page"),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+
+
